24654b4363f523b634f2b575ce3326c45944c93f
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2021-03-04 21:40:29 +1300

    Completion of rough draft for WCR calculations. Logic behind the Working Capital Ratio calculations completed. All functions now work unpolished - raw results are still accurate to what the brief suggested in terms of logic.

--- calculation.js
+++ calculation.js
@@ -38,14 +38,42 @@ function calc_npm(rev_value, exp_value){
 
 function calc_wcr(accounts){
     // Filters and calculations
-    const assets_debit;
-    const assets_credit;
+
+    const debit_assets_filter = accounts.filter(account => account.account_category === 'assets' && account.value_type === 'debit');
+    const debit_assets_arr = debit_assets_filter.filter(asset => asset.account_type === 'current' || asset.account_type === 'bank' || asset.account_type === 'current_accounts_receivable');
+    let assets_debit = 0;
+    for (let item of debit_assets_arr){
+        assets_debit += item.total_value;
+    }
+    console.log("DEBIT ASSETS", assets_debit)
+
+    const credit_assets_filter = accounts.filter(account => account.account_category === 'assets' && account.value_type === 'credit');
+    const credit_assets_arr = credit_assets_filter.filter(asset => asset.account_type === 'current' || asset.account_type === 'bank' || asset.account_type === 'current_accounts_receivable');
+    let assets_credit = 0;
+    for (let item of credit_assets_arr){
+        assets_credit += item.total_value;
+    }
+    console.log("CREDIT ASSETS", assets_credit)
+
+    const debit_liab_filter = accounts.filter(account => account.account_category === 'liability' && account.value_type === 'debit');
+    const debit_liab_arr = debit_liab_filter.filter(asset => asset.account_type === 'current' || asset.account_type === 'current_accounts_payable');
+    let liab_debit = 0;
+    for (let item of debit_liab_arr){
+        liab_debit += item.total_value;
+    }
+    console.log("DEBIT LIAB", liab_debit)
+
+    const credit_liab_filter = accounts.filter(account => account.account_category === 'liability' && account.value_type === 'credit');
+    const credit_liab_arr = credit_liab_filter.filter(asset => asset.account_type === 'current' || asset.account_type === 'current_accounts_payable');
+    let liab_credit = 0;
+    for (let item of credit_liab_arr){
+        liab_credit += item.total_value;
+    }
+    console.log("CREDIT LIAB", liab_credit)
+
     const assets = assets_debit - assets_credit;
-    // Filters and calculations
-    const liab_debit;
-    const liab_credit;
     const liabilities = liab_debit - liab_credit;
-    // Return working capital ratio % value 
+    // // Return working capital ratio % value 
     const wcr_value = (assets / liabilities) * 100;
     return wcr_value;
 }
