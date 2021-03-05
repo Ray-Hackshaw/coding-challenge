b7fea7f542dc9538fa48297001019ea326790581
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2021-03-05 19:13:06 +1300

    Tests for Gross Profit Margin. Empty input and zero division tests for Gross Profit Margin calculation function. Also minor syntax changes with missing semi-colons.

--- test.js
+++ test.js
@@ -5,28 +5,34 @@ const data = JSON.parse(fs.readFileSync('test-data.json'));
 
 // Revenue tests
 describe('Revenue', function(){
-    it("no accounts classified as revenue", function(){
+    it("empty input - no accounts classified as revenue", function(){
         assert.equal(calc.calc_rev([]), 0)
     });
     it("varied accounts - some classified as revenue mixed with others", function(){
         assert.equal(calc.calc_rev(data.data), 15)
-    })
-})
+    });
+});
 
 // Expenses tests
 describe('Expenses', function(){
-    it("no accounts classified as expenses", function(){
+    it("empty input - no accounts classified as expenses", function(){
         assert.equal(calc.calc_exp([]), 0)
     });
     it("varied accounts - some classified as expenses mixed with others", function(){
         assert.equal(calc.calc_exp(data.data), 20)
-    })
+    });
+});
+
+// Gross Profit Margin tests
+
+describe('Gross Profit Margin', function(){
+    it("empty input - no matching accounts", function(){
+        assert.equal(calc.calc_gpm([], 5), 0)
+    });
+    it("zero division - revenue value is zero", function(){
+        assert.equal(calc.calc_gpm(data.data, 0), Infinity)
+    });
 })
-
-// // // Gross Profit Margin tests
-
-// describe('Gross Profit Margin', function(){
-// })
 
 // // // Net Profit Margin tests
 
