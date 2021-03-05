757b3124a64dab3aff73e4a4a79a5aed3db040ad
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2021-03-05 19:03:00 +1300

    test-data.json sample file and more unit tests. Adding a test data JSON for testing purposes to customise input. Adding tests for Revenue and Expenses, testing for multiple accounts of both amongst other accounts.

--- test.js
+++ test.js
@@ -1,31 +1,39 @@
 const assert = require('chai').assert;
+const fs = require('fs');
 const calc = require('./calculation');
+const data = JSON.parse(fs.readFileSync('test-data.json'));
 
 // Revenue tests
-
 describe('Revenue', function(){
     it("no accounts classified as revenue", function(){
         assert.equal(calc.calc_rev([]), 0)
+    });
+    it("varied accounts - some classified as revenue mixed with others", function(){
+        assert.equal(calc.calc_rev(data.data), 15)
     })
 })
 
-
-// // Expenses tests
-
+// Expenses tests
 describe('Expenses', function(){
     it("no accounts classified as expenses", function(){
         assert.equal(calc.calc_exp([]), 0)
+    });
+    it("varied accounts - some classified as expenses mixed with others", function(){
+        assert.equal(calc.calc_exp(data.data), 20)
     })
 })
 
-// // Gross Profit Margin tests
+// // // Gross Profit Margin tests
 
-// describe('Gross Profit Margin')
+// describe('Gross Profit Margin', function(){
+// })
 
-// // Net Profit Margin tests
+// // // Net Profit Margin tests
 
-// describe('Net Profit Margin')
+// describe('Net Profit Margin', function(){
+// })
 
-// // Working Capital Ratio tests
+// // // Working Capital Ratio tests
 
-// describe('Working Capital Ratio')
+// describe('Working Capital Ratio', function(){
+// })
